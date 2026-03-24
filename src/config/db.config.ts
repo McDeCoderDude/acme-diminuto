41dbc2a7301fe4527c6d07ac2006b9fc6433b7a3
Danny McKinney <decoderdude@gmail.com>
2026-03-24 15:04:25 -0400

    refactor: clean up db.config.ts by organizing import statements and updating Agenda initialization

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-import { Agenda} from 'agenda';
-import { MongoBackend } from '@agendajs/mongo-backend';
+import { Agenda } from "agenda";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,26 +10,26 @@ const DB_URI = `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DATA
 const DB_AGENDA_URI = `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_AGENDA_DATABASE}`;
 
 mongoose.connect(DB_URI, {
-    authSource: process.env.MONGODB_DATABASE,
-    user: process.env.MONGODB_USER,
-    pass: process.env.MONGODB_PASSWORD,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000
+  authSource: process.env.MONGODB_DATABASE,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASSWORD,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
 });
 
 const agenda = new Agenda({
-    backend: new MongoBackend({
-        address: DB_AGENDA_URI,
-        collection: 'agenda',
-        options: {
-            authSource: process.env.MONGODB_DATABASE,
-            auth: {
-                username: process.env.MONGODB_USER,
-                password: process.env.MONGODB_PASSWORD,
-            }
-        }
-    })
+  db: {
+    address: DB_AGENDA_URI,
+    collection: "agenda",
+    options: {
+      authSource: process.env.MONGODB_DATABASE,
+      auth: {
+        username: process.env.MONGODB_USER,
+        password: process.env.MONGODB_PASSWORD,
+      },
+    },
+  },
 });
 
 const connection = mongoose.connection;
